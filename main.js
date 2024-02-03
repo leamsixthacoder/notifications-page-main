730b9275ee484c8f2a8a8d7594f05e740e94044b
leamsi-ciber <joseismael13vm@gmail.com>
2024-02-03 11:31:21 -0400

    adding-all-the-properties-to-the-notification-list

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const notifications = [
         id: 1, 
         user: 'Mark Webber',
         interaction: 'reacted to your recent post',
-        topic: 'My first tournament today!',
+        content: 'My first tournament today!',
         timestamp: '1m ago',
         message: '',
         active: true
@@ -11,63 +11,64 @@ const notifications = [
     },
     {
         id: 2, 
-        user: 'Mark Webber',
-        interaction: 'reacted to your recent post',
-        topic: 'My first tournament today!',
-        timestamp: '1m ago',
+        user: 'Angela Gray',
+        interaction: 'followed you',
+        content: '',
+        timestamp: '5m ago',
         message: '',
         active: true
         
     },
     {
         id: 3, 
-        user: 'Mark Webber',
-        interaction: 'reacted to your recent post',
-        topic: 'My first tournament today!',
-        timestamp: '1m ago',
+        user: 'Jacob Thompson',
+        interaction: 'has joined your group',
+        content: 'Chess Club',
+        timestamp: '1 day ago',
         message: '',
         active: true
         
     },
     {
         id: 4, 
-        user: 'Mark Webber',
-        interaction: 'reacted to your recent post',
-        topic: 'My first tournament today!',
-        timestamp: '1m ago',
-        message: '',
-        active: true
+        user: 'Rizky Hasanuddin',
+        interaction: 'sent you a private message',
+        content: '',
+        timestamp: '5 days ago',
+        message: 'Hello, thanks for setting up the Chess Club. I\'ve been a member for a few weeks now and I\'m already having lots of fun and improving my game.',
+        active: false
         
     },
     {
         id: 5, 
-        user: 'Mark Webber',
-        interaction: 'reacted to your recent post',
-        topic: 'My first tournament today!',
-        timestamp: '1m ago',
+        user: 'Kimberly Smith',
+        interaction: 'commented on your picture',
+        content: '',
+        timestamp: '1 week ago',
         message: '',
-        active: true
+        active: false
         
     },
     {
         id: 6, 
-        user: 'Mark Webber',
+        user: 'Nathan Peterson',
         interaction: 'reacted to your recent post',
-        topic: 'My first tournament today!',
-        timestamp: '1m ago',
+        content: '5 end-game strategies to increase your win rate',
+        timestamp: '2 weeks ago',
         message: '',
-        active: true
+        active: false
         
     },
     {
         id: 7, 
-        user: 'Mark Webber',
-        interaction: 'reacted to your recent post',
-        topic: 'My first tournament today!',
-        timestamp: '1m ago',
+        user: 'Anna Kim',
+        interaction: 'left the group',
+        content: 'Chess Club',
+        timestamp: '2 weeks ago',
         message: '',
-        active: true
+        active: false
         
     },
 
 ]
+
