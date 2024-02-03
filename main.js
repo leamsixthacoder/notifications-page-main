a0a9f39ab9e43747cf80c0c245bb627bbc48d451
leamsi-ciber <joseismael13vm@gmail.com>
2024-02-03 10:13:08 -0400

    create-the-list-of-notifications

---
+++ main.js
@@ -0,0 +1,73 @@
+const notifications = [
+    {
+        id: 1, 
+        user: 'Mark Webber',
+        interaction: 'reacted to your recent post',
+        topic: 'My first tournament today!',
+        timestamp: '1m ago',
+        message: '',
+        active: true
+        
+    },
+    {
+        id: 2, 
+        user: 'Mark Webber',
+        interaction: 'reacted to your recent post',
+        topic: 'My first tournament today!',
+        timestamp: '1m ago',
+        message: '',
+        active: true
+        
+    },
+    {
+        id: 3, 
+        user: 'Mark Webber',
+        interaction: 'reacted to your recent post',
+        topic: 'My first tournament today!',
+        timestamp: '1m ago',
+        message: '',
+        active: true
+        
+    },
+    {
+        id: 4, 
+        user: 'Mark Webber',
+        interaction: 'reacted to your recent post',
+        topic: 'My first tournament today!',
+        timestamp: '1m ago',
+        message: '',
+        active: true
+        
+    },
+    {
+        id: 5, 
+        user: 'Mark Webber',
+        interaction: 'reacted to your recent post',
+        topic: 'My first tournament today!',
+        timestamp: '1m ago',
+        message: '',
+        active: true
+        
+    },
+    {
+        id: 6, 
+        user: 'Mark Webber',
+        interaction: 'reacted to your recent post',
+        topic: 'My first tournament today!',
+        timestamp: '1m ago',
+        message: '',
+        active: true
+        
+    },
+    {
+        id: 7, 
+        user: 'Mark Webber',
+        interaction: 'reacted to your recent post',
+        topic: 'My first tournament today!',
+        timestamp: '1m ago',
+        message: '',
+        active: true
+        
+    },
+
+]
